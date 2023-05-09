78c6e11d60f906137fb261bde38a97e28fbe1a14
Giovanni Galloro <ggalloro@users.noreply.github.com>
2023-05-09 09:31:19 +0200

    Minor fixes

--- createrelease.sh
+++ createrelease.sh
@@ -2,12 +2,10 @@
 echo "Building image for 1st release"
 echo
 ###
-skaffold build --default-repo yourrepo \ 
---file-output=artifacts.json
+skaffold build --default-repo=yourrepo --file-output=artifacts.json
 ###
 echo "Creating 1st release"
 echo
-gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline \
---region yourregion --build-artifacts artifacts.json
+gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline --region yourregion --build-artifacts artifacts.json
 ###
 echo "Release creation completed, please promote and approve your release manually""
