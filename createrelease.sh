07e8de1406770e250579e3f49d479da2d5f388ca
sadmar33 <sad.marra@hotmail.com>
2024-03-04 14:14:12 +0000

    Added setup configurations

--- createrelease.sh
+++ createrelease.sh
@@ -2,10 +2,10 @@
 echo "Building image for 1st release"
 echo
 ###
-skaffold build --default-repo=yourrepo --file-output=artifacts.json
+skaffold build --default-repo=europe-west1-docker.pkg.dev/kyr-sandbox-test-sma/repertoire-cicd --file-output=artifacts.json
 ###
 echo "Creating 1st release"
 echo
-gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline --region yourregion --build-artifacts artifacts.json
+gcloud deploy releases create first-release --delivery-pipeline cd-on-gcp-pipeline --region europe-west1 --build-artifacts artifacts.json
 ###
 echo "Release creation completed, please promote and approve your release manually"
