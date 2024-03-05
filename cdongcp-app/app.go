8df1c25800cf9108316579d41b20183058c82d0b
sadmar2 <fouzi.mar33@gmail.com>
2024-03-05 14:02:05 +0000

    new feature

--- cdongcp-app/app.go
+++ cdongcp-app/app.go
@@ -22,7 +22,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "cd-on-gcp app running in target: %s!!\n", os.Getenv("TARGET"))
+	fmt.Fprintf(w, "Sadki cd-on-gcp app running in target: %s!!\n", os.Getenv("TARGET"))
 }
 
 func main() {
